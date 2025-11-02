7b3579753294327f0254251320bb7fc27f39e61d
Keago99 <keatabde@gmail.com>
2025-11-02 19:22:55 +0200

    Add references to index.html divs
    Add references to index.html divs in scripts.js
    using querySelector and querySelectorAll

--- scripts.js
+++ scripts.js
@@ -1,2 +1,30 @@
 
 const rpsButtons = document.querySelectorAll(".rpsButton");
+const currentRound = document.querySelector(".currentRound");
+const mainInfo = document.querySelector(".mainInfo");
+
+let gameround = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    randoChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch(randoChoice){
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            return "this isn't working";
+    }
+}
+
+//player clicks the button
+//game checks what round it is and updates the h3
+//game chooses computer output
+//game compares that to player output
+//screen updates with the result and player/comptuer scores are updated
+//h3 prompts the player to choose again with the round number
